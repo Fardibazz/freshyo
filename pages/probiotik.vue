c1f318b3612747b33fcdc8db0bcf067dece6c2aa
Farah Diba 'Izzati <farahdb01@hgmail.com>
2020-11-24 21:32:46 +0700

    Create probiotics page

---
+++ pages/probiotik.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="mh-100">
+    <AppBanner
+      :cursive="banner.cursive"
+      :img="banner.img"
+      :title="banner.title"
+    />
+
+    <b-container class="mx-auto py-5 wrapper">
+      <nuxt-content :document="page" />
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content }) {
+    let banner = await $content("components/banners").fetch();
+    banner = banner.probioticsBanner;
+
+    const page = await $content("pages/probiotics").fetch();
+
+    return {
+      banner,
+      page
+    };
+  }
+};
+</script>
