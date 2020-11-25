d3e0c60d17482ec5a8889e7ec6a7acc0fe492b47
Farah Diba 'Izzati <farahdb01@hgmail.com>
2020-11-25 19:24:59 +0700

    Make sure data is ready before loading footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,69 +1,71 @@
 <template>
-  <footer class="footer mt-auto">
-    <div class="d-flex text-center justify-content-center" data-aos="flip-up">
-      <b-button
-        href="#top"
-        variant="light"
-        class="bg-white rounded-circle text-muted top"
-      >
-        <ChevronUpIcon />
-      </b-button>
-    </div>
+  <client-only>
+    <footer v-if="footer" class="footer mt-auto">
+      <div class="d-flex text-center justify-content-center" data-aos="flip-up">
+        <b-button
+          class="bg-white rounded-circle text-muted top"
+          to="#top"
+          variant="light"
+        >
+          <ChevronUpIcon />
+        </b-button>
+      </div>
 
-    <b-container fluid class="bg-black pb-4 pt-5 text-white">
-      <b-container class="my-3 mx-auto" data-aos="flip-down">
-        <b-row>
-          <b-col sm="8" md="6">
-            <h3 class="h6 mb-3 mb-sm-4 text-capitalize">
-              {{ footer.locationsLabel }}
-            </h3>
+      <b-container fluid class="bg-black pb-4 pt-5 text-white">
+        <b-container class="my-3 mx-auto" data-aos="flip-down">
+          <b-row>
+            <b-col sm="8" md="6">
+              <h3 class="h6 mb-3 mb-sm-4 text-capitalize">
+                {{ footer.locationsLabel }}
+              </h3>
 
-            <div class="mb-2">
-              <p class="mb-0">
-                {{ footer.locations[0].address }}<br />
-                {{ footer.locations[0].city }}
-              </p>
-            </div>
-          </b-col>
-
-          <b-col sm="4" md="6">
-            <h3 class="h6 mb-3 mb-sm-4 mt-3 mt-sm-0 text-capitalize">
-              {{ footer.hoursLabel }}
-            </h3>
-
-            <b-row>
-              <b-col
-                v-for="(item, index) in footer.hours"
-                :key="index"
-                class="mb-2"
-                lg="4"
-                md="6"
-              >
+              <div class="mb-2">
                 <p class="mb-0">
-                  {{ item.days }}<br />
-                  {{ item.hours }}
+                  {{ footer.locations[0].address }}<br />
+                  {{ footer.locations[0].city }}
                 </p>
-              </b-col>
-            </b-row>
-          </b-col>
-        </b-row>
-      </b-container>
-    </b-container>
+              </div>
+            </b-col>
 
-    <b-container fluid class="bg-dark py-2 text-light">
-      <b-container class="align-items-center d-flex">
-        <p class="mb-0">{{ footer.text || "FRESHYO 2020" }}</p>
+            <b-col sm="4" md="6">
+              <h3 class="h6 mb-3 mb-sm-4 mt-3 mt-sm-0 text-capitalize">
+                {{ footer.hoursLabel }}
+              </h3>
 
-        <ul class="align-items-center d-flex list-unstyled mb-0">
-          <li v-for="(item, index) in accounts" :key="index">
-            <b-link :href="item.link" class="text-light">
-              <component :is="item.icon" class="mx-3 svg" />
-            </b-link>
-          </li>
-        </ul>
+              <b-row>
+                <b-col
+                  v-for="(item, index) in footer.hours"
+                  :key="index"
+                  class="mb-2"
+                  lg="4"
+                  md="6"
+                >
+                  <p class="mb-0">
+                    {{ item.days }}<br />
+                    {{ item.hours }}
+                  </p>
+                </b-col>
+              </b-row>
+            </b-col>
+          </b-row>
+        </b-container>
       </b-container>
-    </b-container>
-  </footer>
+
+      <b-container fluid class="bg-dark py-2 text-light">
+        <b-container class="align-items-center d-flex">
+          <p class="mb-0">{{ footer.text || "FRESHYO 2020" }}</p>
+
+          <ul class="align-items-center d-flex list-unstyled mb-0">
+            <li v-for="(item, index) in accounts" :key="index">
+              <b-link :href="item.link" class="text-light">
+                <component :is="item.icon" class="mx-3 svg" />
+              </b-link>
+            </li>
+          </ul>
+        </b-container>
+      </b-container>
+    </footer>
+  </client-only>
 </template>
 
 <script>
